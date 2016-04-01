efdbf48d5541b895b3eef73c9ba838eafeb0da3f
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-01 12:05:10 -0700

    Refactor MyBaseRowItem.xml to MyBaseItem.xml

---
+++ app/src/main/java/me/makeachoice/movies/adapter/item/MyBaseItem.java
@@ -0,0 +1,6 @@
+package me.makeachoice.movies.adapter.item;
+
+/**
+ * MyBaseItem - is an empty abstract class to act as a base class.
+ */
+public abstract class MyBaseItem {}
--- app/src/main/java/me/makeachoice/movies/adapter/item/MyBaseRowItem.java
+++
@@ -1,6 +0,0 @@
-package me.makeachoice.movies.adapter.item;
-
-/**
- * MyBaseRowItem - is an empty abstract class to act as a base class.
- */
-public abstract class MyBaseRowItem {}
