514ffa490854378dcce35dffaacee7a3e329d3b0
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-01 07:29:04 -0700

    Add Adapter class files to git

---
+++ app/src/main/java/me/makeachoice/movies/adapter/MyBaseAdapter.java
@@ -0,0 +1,22 @@
+package me.makeachoice.movies.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseAdapter;
+
+/**
+ * MyBaseAdapter - abstract class to act as my Base class for all my custom Adapters
+ */
+public abstract class MyBaseAdapter extends BaseAdapter{
+    Context mContext;
+    LayoutInflater mInflater;
+
+    abstract void updateView(int position, View convertView);
+
+    View.OnClickListener mOnClickListener;
+    public void setOnClickListener(View.OnClickListener listener){
+        mOnClickListener = listener;
+    }
+
+}
