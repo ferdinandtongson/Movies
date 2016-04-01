76555979ffe7548672faa5aba472ee0d8d81db1b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-01 13:45:48 -0700

    Add My Fragment base classes and interface to git

---
+++ app/src/main/java/me/makeachoice/movies/fragment/MyFragment.java
@@ -0,0 +1,27 @@
+package me.makeachoice.movies.fragment;
+
+import android.support.v4.app.Fragment;
+import android.widget.ListAdapter;
+
+/**
+ * Fragment abstract used for all my non-list fragments
+ */
+public abstract class MyFragment extends Fragment implements MyFragmentInterface{
+
+    protected int mLayoutId;
+    protected String mServiceName;
+    protected Bridge mBridge;
+
+    abstract public void setLayout(int id);
+    abstract public void setServiceName(String name);
+
+    //Upkeeping Maid class must implement this interface
+    public interface Bridge{
+
+        void onItemClick(int position);
+        ListAdapter getListAdapter();
+    }
+
+
+}
+
---
+++ app/src/main/java/me/makeachoice/movies/fragment/MyListFragment.java
@@ -0,0 +1,29 @@
+package me.makeachoice.movies.fragment;
+
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+/**
+ * MyListFragment abstract class has a set of methods and an Interface class used mainly for trying
+ * to keep an MVP design structure. The abstract class ensure that the communication lines between
+ * the Presenter and the View are established.
+ */
+public abstract class MyListFragment extends ListFragment implements MyFragmentInterface{
+
+    protected int mLayoutId;
+    protected String mServiceName;
+    protected Bridge mBridge;
+
+    abstract public void setLayout(int id);
+    abstract public void setServiceName(String name);
+
+    //Upkeeping Maid class must implement this interface
+    public interface Bridge{
+        void onItemClick(ListView l, View v, int position, long id);
+        ListAdapter getListAdapter();
+        void setListAdapter(ListAdapter adapter);
+    }
+}
