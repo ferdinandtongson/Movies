d963dc5092408eaa0b9b4c664ee1fd1b82aa6511
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-02 06:37:17 -0700

    Add abstract methods onProgressUpdate() and onPostExecute() and Change variables in to AsyncTask<String, Integer, Boolean> in MyWorker class

--- app/src/main/java/me/makeachoice/movies/controller/worker/MyWorker.java
+++ app/src/main/java/me/makeachoice/movies/controller/worker/MyWorker.java
@@ -7,9 +7,30 @@ import me.makeachoice.movies.controller.butler.MyButler;
 /**
  * MyWorker abstract class - used to run threads, network calls or background work to process data
  * before passing it onto the Butler and then general consumption by the View
+ *
+ * MyWorker which extends AsyncTask<Params, Progress, Result>. When extending AsyncTask, you can
+ * modify the type of variables that are received in doInBackground(), onProgressUpdate() and
+ * onPostExecute(). For example:
+ *      AsynTask<String, Integer, Boolean> will result in method calls
+ *      doInBackground(String... params), onProgressUpdate(Integer... values) and
+ *      onPostExecute(Result result)
+ *
+ * doInBackground(String... params); String... means that the doInBackground method can receive
+ * an undefined number of string variable and it will place those variables into a dynamic String
+ * array. So we can call doInBackground("x1", "x2", "x3") and process the variables by using
+ * params[0], params[1] and params[2].
+ *
  */
-public abstract class MyWorker extends AsyncTask {
+public abstract class MyWorker extends AsyncTask<String, Integer, Boolean> {
+
+    //mButler - MyWorker talks directly to the Butler
     MyButler mButler;
 
-    protected abstract Object doInBackground(Object[] params);
+    //abstract method from AsyncTask, see notes above about AsyncTask<Params, Progress, Result>
+    //doInBackground - runs in background thread
+    protected abstract Boolean doInBackground(String... params);
+    //onProgressUpdate - runs in UI thread
+    protected abstract void onProgressUpdate(Integer... values);
+    //onPostExecute - runs in UI thread
+    protected abstract void onPostExecute(Boolean result);
 }
