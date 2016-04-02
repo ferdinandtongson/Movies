0b25ebb76fe265ac5d3005f2924b9282f6a34d77
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-01 17:09:42 -0700

    Change MyWorker class to an abstract class and also to extend AsyncTask

--- app/src/main/java/me/makeachoice/movies/controller/worker/MyWorker.java
+++ app/src/main/java/me/makeachoice/movies/controller/worker/MyWorker.java
@@ -1,11 +1,15 @@
 package me.makeachoice.movies.controller.worker;
 
+import android.os.AsyncTask;
+
 import me.makeachoice.movies.controller.butler.MyButler;
 
 /**
  * MyWorker abstract class - used to run threads, network calls or background work to process data
  * before passing it onto the Butler and then general consumption by the View
  */
-public class MyWorker {
+public abstract class MyWorker extends AsyncTask {
     MyButler mButler;
+
+    protected abstract Object doInBackground(Object[] params);
 }
