297e1a3208b8562d0daea49e35b94a92b1af64b8
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-02 14:15:18 -0700

    Update MoviePosterAdapter to update title textview

--- app/src/main/java/me/makeachoice/movies/adapter/MoviePosterAdapter.java
+++ app/src/main/java/me/makeachoice/movies/adapter/MoviePosterAdapter.java
@@ -6,16 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import me.makeachoice.movies.R;
-import me.makeachoice.movies.adapter.item.MovieItem;
+import me.makeachoice.movies.adapter.item.PosterItem;
 import me.makeachoice.movies.adapter.util.ViewHolder;
 
 /**
-* MoviePosterAdapter extends BaseAdapter class and is used as a List Adapter for the Android
-* GridView widget.
+* MoviePosterAdapter extends BaseAdapter class and is used as a List Adapter to display an image
+* (movie poster) and a title text (movie title)
 *
 * Variables from MyAdapter:
 *      Context mContext
@@ -28,26 +29,38 @@ import me.makeachoice.movies.adapter.util.ViewHolder;
 *
 */
 public class MoviePosterAdapter extends MyAdapter {
+
 /**************************************************************************************************/
-//Default layout has been modifed from TitleSimpleAdapter to use item_titleicon instead
-protected int DEFAULT_LAYOUT_ID = R.layout.item_movie;
-//Default TextView id found in the default layout
-protected int DEFAULT_POSTER_VIEW_ID = R.id.item_poster;
+    //Default layout has been modifed from TitleSimpleAdapter to use item_titleicon instead
+    protected int DEFAULT_LAYOUT_ID = R.layout.item_poster;
+    //Default ImageView id found in the default layout
+    protected int DEFAULT_POSTER_VIEW_ID = R.id.item_poster;
+    //Defaul TextView id found in the default layout
+    protected int DEFAULT_TITLE_VIEW_ID = R.id.item_title;
 
-//mItemLayoutId - id of the layout to be inflated
-protected int mItemLayoutId;
-//mPosterViewId - id of ImageView to display Poster
-protected int mPosterViewId;
+    //mItemLayoutId - id of the layout to be inflated
+    protected int mItemLayoutId;
+    //mPosterViewId - id of ImageView to display Poster
+    protected int mPosterViewId;
+    //mTitleViewId - id of TextView to display title
+    protected int mTitleViewId;
 
-private ArrayList<MovieItem> mItems;
+    //public static variable to be able to index properly the layout and child ids
+    public final static int INDEX_LAYOUT_ID = 0;
+    public final static int INDEX_POSTER_ID = 1;
+    public final static int INDEX_TITLE_ID = 2;
+    public final static int INDEX_MAX = 3;
+
+    //an array list of movie items for the list adapter to consume
+    private ArrayList<PosterItem> mItems;
 
 /**************************************************************************************************/
 /**
  * MoviePosterAdapter - constructor, uses default values for Layout, ImageView
  * @param c - activity context
- * @param items - array list of MovieItem objects; data used to populate view objects
+ * @param items - array list of PosterItem objects; data used to populate view objects
  */
-    public MoviePosterAdapter(Context c, ArrayList<MovieItem> items) {
+    public MoviePosterAdapter(Context c, ArrayList<PosterItem> items) {
         //initialize class variables
         mContext = c;
         mInflater = LayoutInflater.from(c);
@@ -56,24 +69,25 @@ private ArrayList<MovieItem> mItems;
         //use default values for Layout and TextView to display the Title
         mItemLayoutId = DEFAULT_LAYOUT_ID;
         mPosterViewId = DEFAULT_POSTER_VIEW_ID;
+        mTitleViewId = DEFAULT_TITLE_VIEW_ID;
     }
 
     /**
      * MoviePosterAdapter - constructor, accept Keeper values for Layout and ImageView (for Poster)
      * @param c - activity context
-     * @param items - array list of MovieItem objects; data used to populate view objects
-     * @param layoutId - list item layout id
-     * @param posterViewId - child view of list item layout, imageView for thumbnail images
+     * @param items - array list of PosterItem objects; data used to populate view objects
+     * @param ids - an int array holding layout and child id values
      */
-    public MoviePosterAdapter(Context c, ArrayList<MovieItem> items, int layoutId, int posterViewId){
-        //initialize class varialbes
+    public MoviePosterAdapter(Context c, ArrayList<PosterItem> items, int[] ids){
+        //initialize class variable
         mContext = c;
         mInflater = LayoutInflater.from(c);
         mItems = items;
 
         //use Presenter values for Layout and TextView to display the Title
-        mItemLayoutId = layoutId;
-        mPosterViewId = posterViewId;
+        mItemLayoutId = ids[INDEX_LAYOUT_ID];
+        mPosterViewId = ids[INDEX_POSTER_ID];
+        mTitleViewId = ids[INDEX_TITLE_ID];
     }
 
 /**************************************************************************************************/
@@ -88,7 +102,7 @@ private ArrayList<MovieItem> mItems;
     }
 
 /**
- * getItem(int) - returns the object in the ArrayList<T> position; MovieItem Object
+ * getItem(int) - returns the object in the ArrayList<T> position; PosterItem Object
  * @param position - position of list item
  * @return Object - item object at that given position
  */
@@ -138,12 +152,13 @@ private ArrayList<MovieItem> mItems;
  * @param convertView - layout of list item view
  */
     protected void updateView(int position, View convertView){
-        MovieItem item = mItems.get(position);
+        PosterItem item = mItems.get(position);
 
         updatePosterView(item, convertView);
+        updateTitleView(item, convertView);
     }
 
-    private void updatePosterView(MovieItem item, View convertView){
+    private void updatePosterView(PosterItem item, View convertView){
         Log.d("Movies", "MoviePosterAdapter.updatePosterView");
         //get child view using ViewHolder class
         ImageView imgPoster = ViewHolder.get(convertView, mPosterViewId);
@@ -151,26 +166,22 @@ private ArrayList<MovieItem> mItems;
             imgPoster = (ImageView) convertView.findViewById(mPosterViewId);
         }
 
-        //update child view data - title
+        //update child view data - poster image
         imgPoster.setImageResource(item.getPoster());
     }
-/**************************************************************************************************/
 
-    // create a new ImageView for each item referenced by the Adapter
-    /*public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+    private void updateTitleView(PosterItem item, View convertView){
+        //get child view using ViewHolder class
+        TextView txtTitle = ViewHolder.get(convertView, mTitleViewId);
+        if(txtTitle == null){
+            txtTitle = (TextView) convertView.findViewById(mTitleViewId);
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }*/
+        //update child view data - title
+        txtTitle.setText(item.getTitle());
+    }
+
+/**************************************************************************************************/
+
 
 }
