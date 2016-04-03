9046f98312b77bde833e6d1105a80414c83d9b5e
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-03 15:00:25 -0700

    Remove clearAdapter method from PosterMaid

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
@@ -88,10 +88,6 @@ public class PosterMaid extends MyMaid implements PosterFragment.Bridge{
         return mFragment;
     }
 
-    public void clearAdapter(){
-        mListAdapter = null;
-    }
-
 /**************************************************************************************************/
 
 
