d015c5ffff8b515db1bf2a7410ade8ee06a7a097
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-04 14:06:17 -0700

    Capture onBackPressed event so HouseKeeper can be notified of the event in MainActivity

--- app/src/main/java/me/makeachoice/movies/MainActivity.java
+++ app/src/main/java/me/makeachoice/movies/MainActivity.java
@@ -87,6 +87,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        mHouseKeeper.onBackPressed();
+    }
+
 /**************************************************************************************************/
 
 /**************************************************************************************************/
