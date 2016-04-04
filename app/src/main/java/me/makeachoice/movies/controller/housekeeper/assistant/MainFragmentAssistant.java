b4833a77294ba3504f67846cf61bce4966e9f936
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-04 08:51:52 -0700

    Add name identifiers to fragments being managed by FragmentManager

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/assistant/MainFragmentAssistant.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/assistant/MainFragmentAssistant.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 
 import me.makeachoice.movies.R;
+import me.makeachoice.movies.controller.housekeeper.MainKeeper;
 import me.makeachoice.movies.fragment.EmptyFragment;
 import me.makeachoice.movies.fragment.InfoFragment;
 import me.makeachoice.movies.fragment.PosterFragment;
@@ -30,13 +31,23 @@ public class MainFragmentAssistant {
 
 /**************************************************************************************************/
 
-    public void requestFragment(FragmentManager manager, Fragment fragment){
+    public void requestDetailFragment(FragmentManager manager, Fragment fragment,
+                                      String oldFrag, String newFrag){
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.addToBackStack(oldFrag);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.replace(mContainerId, fragment, newFrag);
+        ft.commit();
+    }
+
+
+    public void requestFragment(FragmentManager manager, Fragment fragment, String name){
         if(mHasFragment){
-            replaceFragment(manager, fragment);
+            replaceFragment(manager, fragment, name);
         }
         else{
             mHasFragment = true;
-            addFragment(manager, fragment);
+            addFragment(manager, fragment, name);
         }
     }
 
@@ -44,12 +55,12 @@ public class MainFragmentAssistant {
  * void addFragmentToManager(Fragment) - adds fragment to FragmentManager and commit to activity
  * @param fragment - fragment object to be added
  */
-    private void addFragment(FragmentManager manager, Fragment fragment){
+    private void addFragment(FragmentManager manager, Fragment fragment, String name){
         //begin fragment transaction
         FragmentTransaction ft = manager.beginTransaction();
 
         //add fragment to the fragment container
-        ft.add(mContainerId, fragment);
+        ft.add(mContainerId, fragment, name);
 
         //commit fragment to activity
         ft.commit();
@@ -59,13 +70,13 @@ public class MainFragmentAssistant {
  * void replaceFragmentInManager(int) - replaces a fragment object held by the FragmentManager
  * and commit to activity
  */
-    private void replaceFragment(FragmentManager manager, Fragment fragment){
+    private void replaceFragment(FragmentManager manager, Fragment fragment, String name){
 
         //begin fragment transaction
         FragmentTransaction ft = manager.beginTransaction();
 
         //replace fragment held by the FragmentManager
-        ft.replace(mContainerId, fragment);
+        ft.replace(mContainerId, fragment, name);
 
         //commit fragment to activity
         ft.commit();
