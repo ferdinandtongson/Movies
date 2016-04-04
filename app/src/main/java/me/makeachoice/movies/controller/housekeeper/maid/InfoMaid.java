7085516ce4b90a64b1ddff07ca62e38edede611d
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-04 08:48:42 -0700

    Add methods to be able to pass on the Movie details to Info Fragment

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/InfoMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/InfoMaid.java
@@ -4,8 +4,8 @@ import android.support.v4.app.Fragment;
 import android.widget.ListAdapter;
 
 import me.makeachoice.movies.R;
-import me.makeachoice.movies.fragment.EmptyFragment;
 import me.makeachoice.movies.fragment.InfoFragment;
+import me.makeachoice.movies.model.json.MovieJSON;
 
 /**
  * InfoMaid initializes and takes care of communicating with the Fragment that will display the
@@ -24,7 +24,7 @@ import me.makeachoice.movies.fragment.InfoFragment;
  *      void setListAdapter(ListAdapter) - not used
  *      void onItemClick(...) - not used
  */
-public class InfoMaid extends MyMaid implements EmptyFragment.Bridge{
+public class InfoMaid extends MyMaid implements InfoFragment.Bridge{
 
     private ListAdapter mListAdapter;
     public interface Bridge{
@@ -48,6 +48,22 @@ public class InfoMaid extends MyMaid implements EmptyFragment.Bridge{
 /**************************************************************************************************/
     //LAYOUT_INFO_FRAGMENT - layout used by Info Fragment
     private final static int LAYOUT_INFO_FRAGMENT = R.layout.info_fragment;
+
+    //CHILD_TXT_TITLE - textView child for title of movie
+    private final static int CHILD_TXT_TITLE = R.id.txt_title;
+    //CHILD_IMG_POSTER - imageView child for poster image of movie
+    private final static int CHILD_IMG_POSTER = R.id.img_poster;
+    //CHILD_TXT_RELEASE - textView child for release date of movie
+    private final static int CHILD_TXT_RELEASE = R.id.txt_release;
+    //CHILD_TXT_CAST - textView child for cast members of movie
+    private final static int CHILD_TXT_CAST = R.id.txt_cast;
+    //CHILD_RTB_RATING - ratingBarView child for visual display of rating
+    private final static int CHILD_RTB_RATING = R.id.rtb_rating;
+    //CHILD_TXT_RATING - textView child for rating percentage of movie
+    private final static int CHILD_TXT_RATING = R.id.txt_rating;
+    //CHILD_TXT_OVERVIEW - textView child for overview, plot, of movie
+    private final static int CHILD_TXT_OVERVIEW = R.id.txt_overview;
+
 /**************************************************************************************************/
 
 /**************************************************************************************************/
@@ -63,6 +79,12 @@ public class InfoMaid extends MyMaid implements EmptyFragment.Bridge{
 
         //send Maid Name to fragment
         fragment.setServiceName(mName);
+
+        Integer[] ids = {CHILD_TXT_TITLE, CHILD_TXT_RELEASE, CHILD_TXT_CAST,
+                CHILD_TXT_RATING, CHILD_TXT_OVERVIEW};
+
+        fragment.setTxtViewIds(ids);
+        fragment.setChildIds(CHILD_IMG_POSTER, CHILD_RTB_RATING);
 
         return fragment;
     }
@@ -110,6 +132,30 @@ public class InfoMaid extends MyMaid implements EmptyFragment.Bridge{
         //is empty
     }
 
+    public String[] getTxtValues(){
+        return mValues;
+    }
+
+//TODO - methods in InfoMaid are a bit rough
+    public MovieJSON.MovieDetail getMovie(){
+        return mItem;
+    }
+
+    private String[] mValues;
+    private MovieJSON.MovieDetail mItem;
+    public void setMovie(MovieJSON.MovieDetail item){
+        mItem = item;
+        String [] values = {item.getOriginalTitle(), item.getReleaseDate(),
+                "Placeholder for casting info",
+                "User Rating: " + item.getVoteAverage().toString(),
+                item.getOverview()};
+
+        mValues = values;
+        mRating = item.getVoteAverage();
+    }
+
+
+    private Double mRating;
 /**************************************************************************************************/
 
 
