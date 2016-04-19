c43cdacfb1991fff7582d0496cfcfb6206d52399
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-19 15:34:09 -0700

    Add EmptyPage to git

---
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/page/EmptyPage.java
@@ -0,0 +1,23 @@
+package me.makeachoice.movies.controller.housekeeper.page;
+
+import me.makeachoice.movies.R;
+
+/**
+ * EmptyPage contains constant definitions for all the resources used by any of the "Empty"
+ * classes underneath the housekeeper.
+ */
+public class EmptyPage {
+
+
+/**************************************************************************************************/
+// Fragment layout ids used to display an empty fragment, from empty_fragment.xml
+
+    //EMPTY_FRAGMENT_LAYOUT_ID is the fragment layout used to display a bunch of movie posters
+    public static final int EMPTY_FRAGMENT_LAYOUT_ID = R.layout.empty_fragment;
+
+    //EMPTY_TXT_ID is the textView displaying the "empty" message"
+    public static final int EMPTY_TXT_ID = R.id.txt_empty;
+
+/**************************************************************************************************/
+
+}
