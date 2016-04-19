67299a8565034743af689839ca0815e48989ed36
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-19 13:19:51 -0700

    Implement RecyclerView design change in PosterMaid

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
@@ -1,88 +1,228 @@
 package me.makeachoice.movies.controller.housekeeper.maid;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
 
-import me.makeachoice.movies.R;
-import me.makeachoice.movies.controller.housekeeper.MainKeeper;
+import java.util.ArrayList;
+
+import me.makeachoice.movies.adapter.item.PosterItem;
+import me.makeachoice.movies.controller.housekeeper.GridAutofitLayoutManager;
+import me.makeachoice.movies.controller.housekeeper.maid.staff.PosterStaff;
+import me.makeachoice.movies.controller.housekeeper.page.PosterPage;
+import me.makeachoice.movies.controller.housekeeper.recycler.PosterRecycler;
 import me.makeachoice.movies.fragment.PosterFragment;
+import me.makeachoice.movies.model.json.MovieJSON;
 
 /**
  * PosterMaid initializes and takes care of communicating with the Fragment that hold the
  * list of poster Movies the user can select from.
  *
- * Its main purpose is to handle events and request from the Fragment and if the Maid cannot handle
- * a request or an event, it will pass it onto the HouseKeeper.
+ * Its main purpose is to upkeep and handle events and request from the Fragment and if the Maid
+ * cannot handle a request or an event, it will pass it onto the HouseKeeper.
  *
  * The PosterMaid is only aware of the Fragment and the views at the fragment level. It is NOT
- * aware of the view above it (the Activity containing the Fragment) or below it (the View Items in
- * a ListView or GridView, for example).
+ * aware of the view above it (the Activity containing the Fragment).
+ *
+ * It uses other classes to assist in the upkeeping of the Fragment:
+ *      PosterFragment - handles the Fragment lifecycle
+ *      PosterRecycler - RecyclerView adapter used to display the list of movie posters
+ *      PosterPage - holds all static resources (layout id, view ids, etc)
+ *      PosterStaff - prepares data model for consumption for the Poster View
  *
  * Variables from MyMaid:
  *      MyHouseKeeper mHouseKeeper
  *      String mName
  *      Fragment mFragment
  *
+ * Methods from MyMaid:
+ *      void initFragment()
+ *      Fragment getFragment()
+ *
  * Implements PosterFragment.Bridge
- * //TODO - need to review implementations
- *      ListAdapter getListAdapter() - Fragments' access to the ListAdapter
- *      void setListAdapter(ListAdapter) - receives the ListAdapter from Boss to be displayed
- *      void onItemClick(...) - list item click event
+ *      View createView(LayoutInflater, ViewGroup, Bundle);
+ *      void createActivity(Bundle, View);
+ *
+ * Implements PosterRecycler.Bridge
+ *      Context getActivityContext()
+ *
+ * Bridge Interface:
+ *      void registerFragment(String, Fragment)
+ *
  */
-public class PosterMaid extends MyMaid implements PosterFragment.Bridge{
+public class PosterMaid extends MyMaid implements PosterFragment.Bridge, PosterRecycler.Bridge{
 
-    private ListAdapter mListAdapter;
+/**************************************************************************************************/
+/**
+ * Class Variables
+ *      Bridge mBridge - class implementing Bridge interface
+ *      PosterRecycler mRecycler - manages item views for the RecyclerView used in the Fragment
+ *      PosterStaff mStaff - processes data to be consumed by the Fragment
+ *      ArrayList<PosterItem> mPosterItems - data to be consumed by the Fragment
+ *
+ * Interface:
+ *      Bridge
+ */
+/**************************************************************************************************/
+
+    //mBridge - class implementing Bridge, typically a MyHouseKeeper class
+    private Bridge mBridge;
+
+    //mRecycler - manages item views for the RecyclerView used in the Fragment
+    private PosterRecycler mRecycler;
+
+    //mStaff - processes data to be consumed by the Fragment
+    private PosterStaff mStaff;
+
+    //mPosterItems - array list of data ready to be consumed by the Fragment
+    private ArrayList<PosterItem> mPosterItems;
+
+
+    //Implemented communication line to any MyHouseKeeper class
     public interface Bridge{
-        //Interface methods needed to be implemented by the instantiating class
-        ListAdapter requestPosterAdapter();
-        void onItemClick(ListView l, View v, int position, long id);
+        //get Context of current Activity
+        Context getActivityContext();
+        //registers the Fragment the Maid class is responsible for
         void registerFragment(String key, Fragment fragment);
     }
 
-    private Bridge mBridge;
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * PosterMaid - constructor
+ * @param bridge - class implementing Bridge interface, typically a MyHouseKeeper class
+ * @param name - name given to PosterMaid
+ */
     public PosterMaid(Bridge bridge, String name){
+        //class implementing Bridge interface
         mBridge = bridge;
+
+        //service name given to PosterMaid
         mName = name;
 
+        //registers fragment PosterMaid is assigned to maintain
         mBridge.registerFragment(name, getFragment());
+
+        //initialize fragment to be maintained
+        initFragment();
+
+        //initialize Staff
+        mStaff = new PosterStaff();
+
+        //initialize RecyclerView Adapter
+        mRecycler = new PosterRecycler(this);
     }
 
 /**************************************************************************************************/
+
+/**************************************************************************************************/
 /**
- * Variables used for initializing Fragments
+ * Getters:
+ *      Context getActivityContext() - required as part of PosterRecycler.Bridge interface
+ *
+ * Setters:
+ *      - None -
  */
 /**************************************************************************************************/
-    //LAYOUT_POSTER_FRAGMENT - layout id used by Poster Fragment
-    private final static int LAYOUT_POSTER_FRAGMENT = R.layout.poster_fragment;
-    //POSTER_CHILD_GRID_VIEW - child view in poster fragment layout, gridView child
-    private final static int POSTER_CHILD_GRID_VIEW = R.id.gridview;
+/**
+ * Context getActivityContext() - get current Activity context
+ * @return - current Activity context
+ */
+    public Context getActivityContext(){
+        return mBridge.getActivityContext();
+    }
+
 /**************************************************************************************************/
 
 /**************************************************************************************************/
 /**
- * void initFragment() - initialize Fragment; set layout and child view ids and maid name
+ * Fragment related methods. Both createView(...) and createActivity(...) are called by
+ * PosterFragment when onCreateView(...) and onCreateActivity(...) are called in that class.
+ *
  */
-    protected Fragment initFragment(){
+/**************************************************************************************************/
+/**
+ * void initFragment() - initialize Fragment, give name of Maid to fragment
+ */
+    protected void initFragment(){
         //create PosterFragment
         PosterFragment fragment = new PosterFragment();
-
-        //send layout id to PosterFragment
-        fragment.setLayout(LAYOUT_POSTER_FRAGMENT);
-
-        //send child view id, gridView
-        fragment.setGridViewId(POSTER_CHILD_GRID_VIEW);
 
         //send Maid name to fragment
         fragment.setServiceName(mName);
 
-        return fragment;
+        //save fragment as class variable
+        //TODO - check if needed
+        mFragment = fragment;
     }
 
+/**
+ * View createView(LayoutInflater, ViewGroup, Bundle) - is called by PosterFragment when
+ * onCreateView(...) is called in that class. Prepares the Fragment View to be presented.
+ * @param inflater - layoutInflater to inflate the xml fragment layout resource file
+ * @param container - view that will hold the fragment view
+ * @param savedInstanceState - saved instance states
+ * @return - view of fragment is ready
+ */
+    public View createView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState){
+
+        //inflate fragment from the xml fragment layout resource file
+        View v = inflater.inflate(PosterPage.POSTER_FRAGMENT_LAYOUT_ID, container, false);
+
+        //return fragment
+        return v;
+    }
+
+/**
+ * void createActivity(Bundle, View) - is called by PosterFragment when onCreateActivity(...)
+ * is called in that class. Sets child views in fragment before being seen by the user
+ * @param savedInstanceState - saved instance states
+ * @param layout - layout where child views reside
+ */
+    public void createActivity(Bundle savedInstanceState, View layout){
+
+        //get RecyclerView
+        RecyclerView recGrid = (RecyclerView)layout.findViewById(PosterPage.POSTER_REC_ID);
+        //setHasFixedSize to true because 1)is true and 2)for optimization
+        recGrid.setHasFixedSize(true);
+
+        //set onItemTouchListener for items in RecyclerView
+        //TODO - need to fix and relocate onItemClick event logic
+        recGrid.addOnItemTouchListener(
+                new RecyclerItemClickListener(mBridge.getActivityContext(),
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Log.d("Movies", "PosterFragment.onItemClick: " + position);
+                            }
+                        })
+        );
+
+        //create LayoutManager for RecyclerView, in this case a Grid type LayoutManager
+        //TODO - need to change 240 to a dynamic variable
+        GridAutofitLayoutManager manager =
+                new GridAutofitLayoutManager(mBridge.getActivityContext(), 240);
+
+        //set layout manager of RecyclerView
+        recGrid.setLayoutManager(manager);
+
+        //set RecyclerAdapter of RecyclerView
+        recGrid.setAdapter(mRecycler);
+    }
+
+
+
+    //TODO - check if can be removed
     public Fragment getFragment(){
         if(mFragment == null){
-            mFragment = initFragment();
+            initFragment();
         }
 
         return mFragment;
@@ -90,62 +230,26 @@ public class PosterMaid extends MyMaid implements PosterFragment.Bridge{
 
 /**************************************************************************************************/
 
-
-/**************************************************************************************************/
-
 /**************************************************************************************************/
 /**
- * SimpleListFragment.Bridge interface implementation.
- *      ListAdapter getListAdapter() - Fragments' access to the ListAdapter
- *      void setListAdapter() - used to ensure that the Maid class uses the setListAdapter method
- *      void onItemClick() - list item click event
+ * Public Methods:
+ *      updatePosters(MovieJSON)
+ *      getActivityContext()
  */
 /**************************************************************************************************/
 /**
- * ListAdapter getListAdapter() - Fragment can get access to the ListAdapter
- * @return ListAdapter - returns a list adapter created by the Boss
+ * void updatePosters(MovieJSON) - called by HouseKeep to inform Maid that changes to the data
+ * being displayed in the fragment has occurred.
+ * @param model - MovieJSON data model
  */
-    public ListAdapter getListAdapter(){
-        //checks if ListAdapter is null
-        if(mListAdapter == null){
-            //if null, request ListAdapter from Boss
-            mListAdapter = mBridge.requestPosterAdapter();
-        }
+    public void updatePosters(MovieJSON model){
 
-        //return list adapter to calling fragment
-        return mListAdapter;
-    }
+        mStaff.consumeModel(model);
 
-/**
- * void setListAdapter(ListAdapter) - set the ListAdapter the fragment is going to use, if any.
- * @param adapter - ListAdapter to be consumed by the fragment
- */
-    public void setListAdapter(ListAdapter adapter){
-        mListAdapter = adapter;
-    }
+        mPosterItems = mStaff.getPosterItems();
 
-
-/**
- * void onItemClick(int) - event listener call by the fragment when an app item has been clicked
- * @param position - list position of item clicked
- */
-    public void onItemClick(ListView l, View v, int position, long id){
-        //do not use
+        mRecycler.setPosters(mPosterItems);
     }
 
 /**************************************************************************************************/
-
-
-    /**
-     * void onItemClick(int) - event listener call by the fragment when an app item has been clicked
-     * @param position - list position of item clicked
-     */
-    public void onItemClick(int position){
-        //TODO - temporary workaround for PosterFragment bug
-        //temporary use of Bridge method as a work around for Empty PosterFragment bug
-        mFragment = initFragment();
-        ((MainKeeper)mBridge).onItemClick();
-    }
-
-
 }
