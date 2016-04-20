b91ccbb7b6d2fdffc009bc49146540e7b434520f
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-19 21:39:00 -0700

    Clean up abstract methods and variables in MyMaid abstract class

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/MyMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/MyMaid.java
@@ -2,18 +2,38 @@ package me.makeachoice.movies.controller.housekeeper.maid;
 
 import android.support.v4.app.Fragment;
 
-import me.makeachoice.movies.controller.housekeeper.MyHouseKeeper;
 
 /**
- * MyMaid abstract class - used to directly communicate with Fragments and with the managing
+ * MyMaid abstract class used to directly communicate with Fragments and with the managing
  * MyHouseKeeper class
  */
 public abstract class MyMaid {
-    MyHouseKeeper mHouseKeeper;
+
+/**************************************************************************************************/
+/**
+ * Class Variables
+ *      String mName - name of the instantiated Maid class
+ *      Fragment mFragment - fragment being maintained by the Maid
+ *
+/**************************************************************************************************/
+
+    //mName - name of the Maid class
     String mName;
+
+    //mFragment - fragment being maintained by the Maid
     Fragment mFragment;
 
+/**************************************************************************************************/
+/**
+ * Abstract Methods:
+ *      initFragment()
+ */
+/**************************************************************************************************/
+/**
+ * void initFragment - initialize fragment class
+ */
     protected abstract Fragment initFragment();
-    public abstract Fragment getFragment();
+
+/**************************************************************************************************/
 
 }
