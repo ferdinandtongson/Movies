0cfde210cb1e85bb654f9f4a5f191a4f89b62144
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-21 09:53:45 -0700

    Clean up code and add NetworkValet functionality to Boss

--- app/src/main/java/me/makeachoice/movies/controller/Boss.java
+++ app/src/main/java/me/makeachoice/movies/controller/Boss.java
@@ -2,8 +2,6 @@ package me.makeachoice.movies.controller;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 
 import java.util.HashMap;
 
@@ -12,6 +10,7 @@ import me.makeachoice.movies.controller.housekeeper.MainKeeper;
 import me.makeachoice.movies.controller.housekeeper.MyHouseKeeper;
 import me.makeachoice.movies.controller.housekeeper.helper.MainHelper;
 import me.makeachoice.movies.controller.housekeeper.maid.MyMaid;
+import me.makeachoice.movies.controller.valet.NetworkValet;
 import me.makeachoice.movies.model.MovieJSON;
 
 /**
@@ -20,56 +19,64 @@ import me.makeachoice.movies.model.MovieJSON;
  * prevented; in MVC (Model-View-Controller) model, the Model and View can communicate
  */
 
-//TODO - stage2 Notes
-/**
- * ActivityContext cannot be passed into the constructor of any class. Check all classes for any
- * references to ActivityContext. Place reference only in Boss since Boss will be updated the most
- * current ActivityContext whenever a new activity is created.
- *
- * The Butler class needs to handle more responsibility for cleaning, clearing, and checking for
- * data models; reference point and all that. Workers should not hold reference points for data. As
- * well, the Butler should be able to check on the status of any running threads so need to create
- * a registry for running threads and push and pop them when they start and finish.
- *
- * Shutdown procedure from front to back is not implemented at all. Assuming that Android will take
- * care of shutting down processes is not a very clever idea. Implement shutdown procedures for all
- * Control classes and thread first.
- *
- *
- */
 public class Boss extends Application{
 
 /**************************************************************************************************/
+/**
+ * Class Variables:
+ *      Context mActivityContext - current Activity Context on display
+ *      MovieButler mButler - Butler class taking care of Movie data
+ *      NetworkValet mNetworkValet - in charge of checking for network connectivity
+ *      int mMovieRequest - current list of movies requested
+ *      HashMap<Integer,MyHouseKeeper> mHouseKeeperRegistry - registered HouseKeepers
+ *      HashMap<Integer,MyMaid> mMaidRegistry - registered Maids
+ */
+/**************************************************************************************************/
 
-    //Context of the UI Activity (In this case we are only using one Activity)
+    //mActivityContext is the current UI Activity (In this case we are only using one Activity)
     Context mActivityContext;
-    //Butler class the will take care of preparing Movie data for consumption
+
+    //mButler will take care of preparing Movie data for consumption
     MovieButler mButler;
+
+    //mNetworkValet is in charge of checking for network connectivity
+    NetworkValet mNetworkValet;
+
     //mMovieRequest - current list of movies requested
     private int mMovieRequest;
 
+    //mHouseKeeperRegistry - HashMap of instantiated HouseKeeper classes being used by the Boss
+    private HashMap<Integer, MyHouseKeeper> mHouseKeeperRegistry = new HashMap<>();
+
+    //mMaidRegistry - HashMap of instantiated Maid classes being used by HouseKeepers
+    private HashMap<Integer, MyMaid> mMaidRegistry = new HashMap<>();
+
+
 /**************************************************************************************************/
 
 /**************************************************************************************************/
 /**
- * void setActivityContext(Context) - this method is called at in the onCreate of an Activity
- * @param ctx - Activity context
+ * Getters:
+ *      Context getActivityContext() - get current Activity Context
+ *      MovieJson getMovies(int) - get list of JSON movie data
+ *      MyHouseKeeper getHouseKeeper(Integer) - get houseKeeper from registry
+ *      MyMaid getMaid(Integer) - get Maid from registry
+ *
+ * Setters:
+ *      - None -
  */
-    public void setActivityContext(Context ctx){
-        //set Activity context - in this case there is only MainActivity
-        mActivityContext = ctx;
-
-        //check if Butler is awake
-        if(mButler == null){
-            //initialize MovieButler
-            mButler = new MovieButler(this);
-        }
-
-        mButler.setActivityContext(ctx);
-    }
+/**************************************************************************************************/
+/**
+ * Context getActivityContext - get current Activity Context
+ * @return - context of current activity
+ */
+    public Context getActivityContext(){
+    return mActivityContext;
+}
 
 /**
- * MovieJSON getModel(int) - request access to the JSON movie data
+ * MovieJSON getModel(int) - request access to the JSON movie data. If null or is a new request,
+ * Butler will start an AsyncTask to get new movie data.
  * @param request - type of movie data requested (most popular or highest rated)
  * @return - an array list of movie data
  */
@@ -88,9 +95,98 @@ public class Boss extends Application{
         return mButler.getModel();
     }
 
-    public void clearMovies(){
-        mButler.stopWorking();
-        mButler.clearModel();
+/**
+ * MyHouseKeeper getHouseKeeper(String) - request a reference to a HouseKeeper
+ * @param key - HouseKeeper Name
+ * @return - sends HouseKeeper reference
+ */
+    public MyHouseKeeper getHouseKeeper(Integer key){
+
+        //request houseKeeper from registry
+        MyHouseKeeper houseKeeper = mHouseKeeperRegistry.get(key);
+
+        //check if houseKeeper is available
+        if(houseKeeper == null){
+            //if not available, wake houseKeeper
+            houseKeeper = startHouseKeeper(key);
+        }
+
+        //return houseKeeper
+        return houseKeeper;
+    }
+
+/**
+ * MyMaid getMaid(String) - request a reference to a Maid
+ * @param key - Maid name
+ * @return - sends Maid reference
+ */
+    public MyMaid getMaid(Integer key){
+        //send Maid
+        return mMaidRegistry.get(key);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Public Methods:
+ *      void registerHouseKeeper(int,MyHouseKeeper) - adds HouseKeeper to registry
+ *      void registerMaid(int,MyMaid) - adds Maid to registry
+ *      void activityCreated(Context) - notifies Boss that onCreate() has been called in Activity
+ *      boolean checkNetwork() - checks network connection of phone
+ *      void downloadComplete() - called by Butler when network download has completed
+ */
+/**************************************************************************************************/
+/**
+ * void registerHouseKeeper(String, MyHouseKeeper) - registers HouseKeepers used by the Boss
+ * @param key - HouseKeeper name
+ * @param keeper - HouseKeeper class (in charge of Activity objects)
+ */
+    public void registerHouseKeeper(int key, MyHouseKeeper keeper){
+        //register HouseKeeper
+        mHouseKeeperRegistry.put(key, keeper);
+    }
+
+/**
+ * void registerMaid(String, MyMaid) - registers Maids being used by HouseKeepers
+ * @param key - Maid name
+ * @param maid - Maid class (in charge of maintaining fragments)
+ */
+    public void registerMaid(Integer key, MyMaid maid){
+        //register Maid
+        mMaidRegistry.put(key, maid);
+    }
+
+/**
+ * void activityCreated(Context) - called when the onCreate() of an Activity is called, stores the
+ * Activity Context and makes sure the Butler for activity is awake.
+ * @param ctx - Activity context
+ */
+    public void activityCreated(Context ctx){
+        //set Activity context - in this case there is only MainActivity
+        mActivityContext = ctx;
+
+        //check if Butler is awake
+        if(mButler == null){
+            //initialize MovieButler
+            mButler = new MovieButler(this);
+        }
+    }
+
+/**
+ * boolean hasNetworkConnection() - checks if the phone has network connection
+ * @return - true or false, will create alert dialog if false
+ */
+    public boolean hasNetworkConnection(){
+
+        //check if network valet is awake
+        if(mNetworkValet == null){
+            //start networkValet
+            mNetworkValet = new NetworkValet(this);
+        }
+
+        //return network connection status
+        return mNetworkValet.hasConnection();
     }
 
 
@@ -98,7 +194,8 @@ public class Boss extends Application{
  * void downloadMovieDataComplete() - communication channel used by Butlers to let the Boss
  * know when an AsyncTask thread has completed (in this case for MovieData)
  */
-    public void downloadMovieDataComplete(){
+    public void updateMainActivity(){
+        //TODO - very rigid, need to massage
         //instantiate HouseKeeper that will maintain the Main Activity
         MainKeeper keeper = (MainKeeper)mHouseKeeperRegistry.get(MainHelper.NAME_ID);
         //tells the HouseKeeper to prepare the fragments the Activity will use
@@ -106,65 +203,22 @@ public class Boss extends Application{
     }
 
 
-/**************************************************************************************************/
-
 
 /**************************************************************************************************/
 
-    //mHouseKeeperRegistry - HashMap of instantiated HouseKeeper classes being used by the Boss
-    private HashMap<Integer, MyHouseKeeper> mHouseKeeperRegistry = new HashMap<>();
-
-    //mMaidRegistry - HashMap of instantiated Maid classes being used by HouseKeepers
-    private HashMap<Integer, MyMaid> mMaidRegistry = new HashMap<>();
 
 /**************************************************************************************************/
-    /**
-     * void registerMaid(String, MyMaid) - registers Maids being used by HouseKeepers
-     * @param key - Maid name
-     * @param maid - Maid class (in charge of maintaining fragments)
-     */
-    public void registerMaid(Integer key, MyMaid maid){
-        //register Maid
-        mMaidRegistry.put(key, maid);
-    }
+/**
+ * Private Methods:
+ *      MyHouseKeeper startHouseKeeper(Integer) - starts requested HouseKeeper
+ */
 
-    /**
-     * MyMaid getMaid(String) - request a reference to a Maid
-     * @param key - Maid name
-     * @return - sends Maid reference
-     */
-    public MyMaid getMaid(Integer key){
-        //send Maid
-        return mMaidRegistry.get(key);
-    }
-
-    /**
-     * void registerHouseKeeper(String, MyHouseKeeper) - registers HouseKeepers used by the Boss
-     * @param key - HouseKeeper name
-     * @param keeper - HouseKeeper class (in charge of Activity objects)
-     */
-    public void registerHouseKeeper(int key, MyHouseKeeper keeper){
-        //register HouseKeeper
-        mHouseKeeperRegistry.put(key, keeper);
-    }
-
-    /**
-     * MyHouseKeeper getHouseKeeper(String) - request a reference to a HouseKeeper
-     * @param key - HouseKeeper Name
-     * @return - sends HouseKeeper reference
-     */
-    public MyHouseKeeper getHouseKeeper(Integer key){
-
-        MyHouseKeeper houseKeeper = mHouseKeeperRegistry.get(key);
-
-        if(houseKeeper == null){
-            houseKeeper = startHouseKeeper(key);
-        }
-
-
-        return houseKeeper;
-    }
-
+/**************************************************************************************************/
+/**
+ * MyHouseKeeper startHouseKeeper(Integer) - starts requested HouseKeeper and add to registry
+ * @param key - id number of HouseKeeper
+ * @return - requested HouseKeeper or null
+ */
     private MyHouseKeeper startHouseKeeper(Integer key){
         if(key == MainHelper.NAME_ID){
             MainKeeper keeper = new MainKeeper(this);
@@ -175,25 +229,6 @@ public class Boss extends Application{
         return null;
     }
 
-    public Context getActivityContext(){
-        return mActivityContext;
-    }
 
-    public boolean hasConnectivity(){
-        //get Connectivity Manger
-        ConnectivityManager connMgr = (ConnectivityManager)
-                mActivityContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        //get access to network information from phone
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        //check if we have connection
-        if(networkInfo != null && networkInfo.isConnected()) {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 
 }
