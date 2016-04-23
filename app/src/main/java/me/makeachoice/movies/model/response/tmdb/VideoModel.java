2bd5301f2e31e395a764c2918c33fcd598933f00
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-23 06:28:04 -0700

    Add VideoModel and VideosResponse to git

---
+++ app/src/main/java/me/makeachoice/movies/model/response/tmdb/VideoModel.java
@@ -0,0 +1,13 @@
+package me.makeachoice.movies.model.response.tmdb;
+
+/**
+ * Created by Usuario on 4/23/2016.
+ */
+public class VideoModel {
+    public String id;
+    public String key;
+    public String name;
+    public String site;
+    public int size;
+    public String type;
+}
---
+++ app/src/main/java/me/makeachoice/movies/model/response/tmdb/VideosResponse.java
@@ -0,0 +1,16 @@
+package me.makeachoice.movies.model.response.tmdb;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Usuario on 4/23/2016.
+ */
+public class VideosResponse {
+    public int id;
+
+    @SerializedName("results")
+    public ArrayList<VideoModel> videos;
+
+}
