bdd5fb162e7083c9653e7ef1278c9c5db28840cb
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-24 12:46:47 -0700

    Add Bridge interface to abstract Base Class MyMaid

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/MyMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/MyMaid.java
@@ -1,6 +1,9 @@
 package me.makeachoice.movies.controller.housekeeper.maid;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+
+import me.makeachoice.movies.controller.housekeeper.helper.EmptyHelper;
 
 
 /**
@@ -29,6 +32,27 @@ public abstract class MyMaid {
  * void initFragment - initialize fragment class
  */
     protected abstract Fragment initFragment();
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Bridge Interface:
+ *      Context getActivityContext() - get current Activity Context
+ *      void registerFragment(Integer, Fragment) - register fragment Maid is maintaining
+ */
+/**************************************************************************************************/
+
+    //mBridge - class implementing Bridge, typically a MyHouseKeeper class
+    protected Bridge mBridge;
+
+    //Implemented communication line to any MyHouseKeeper class
+    public interface Bridge{
+        //get Context of current Activity
+        Context getActivityContext();
+        //Interface methods needed to be implemented by the instantiating class
+        void registerFragment(Integer key, Fragment fragment);
+    }
 
 /**************************************************************************************************/
 
