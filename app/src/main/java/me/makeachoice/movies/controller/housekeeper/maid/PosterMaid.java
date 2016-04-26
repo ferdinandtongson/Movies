15d9865bd440cbbb38e58a11b0bb6c41b929066b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-26 07:06:53 -0700

    Remove PosterStaff reference in PosterMaid

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/maid/PosterMaid.java
@@ -15,7 +15,6 @@ import me.makeachoice.movies.adapter.item.PosterItem;
 import me.makeachoice.movies.controller.housekeeper.recycler.RecyclerItemClickListener;
 import me.makeachoice.movies.util.GridAutofitLayoutManager;
 import me.makeachoice.movies.controller.housekeeper.helper.PosterHelper;
-import me.makeachoice.movies.controller.housekeeper.maid.staff.PosterStaff;
 import me.makeachoice.movies.controller.housekeeper.recycler.PosterRecycler;
 import me.makeachoice.movies.fragment.PosterFragment;
 
