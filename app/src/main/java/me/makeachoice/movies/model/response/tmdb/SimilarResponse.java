d398cd28a596ad748b2c6863499d8b47e3ef1094
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-29 10:35:08 -0700

    Add comments to SimilarResponse

--- app/src/main/java/me/makeachoice/movies/model/response/tmdb/SimilarResponse.java
+++ app/src/main/java/me/makeachoice/movies/model/response/tmdb/SimilarResponse.java
@@ -5,15 +5,69 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 
 /**
- * Created by Usuario on 4/23/2016.
+ * SimilarResponse is used by Gson to parse Movie data similar to the given movie
+ *
+ * API Call to TMDB:
+ *      http://api.themoviedb.org/3/movie/movieID/similar?api_key=xxx (movieID = id number of movie)
+ *      http://api.themoviedb.org/3/movie/157336/similar?api_key=xxx
+ * JSON Response:
+ *      root{1}
+ *          array{2}
+ *              page:
+ *              results[]
+ *              total_pages:
+ *              total_results:
+ *
  */
 public class SimilarResponse {
 
-    @SerializedName("results")
-    public ArrayList<MovieModel> movies;
+/**************************************************************************************************/
+/**
+ * Class Variables:
+ *      ArrayList<MovieModel> - list of MovieModels that are similar to the given movie
+ *
+ * NOTE: Variable names need to match the JSON response for GSON to properly map the data into
+ * VideoResponse. If using a different variable name, use @SerializedName("")
+ */
+/**************************************************************************************************/
 
+    //movies - list of movies that are similar to the given movie
+    @SerializedName("results")
+    private ArrayList<MovieModel> movies;
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * SimilarResponse - constructor, initialize ArrayLists (potential bug if not initialized)
+ */
     public SimilarResponse(){
         movies = new ArrayList<>();
     }
+
+/**************************************************************************************************/
+/**
+ * Getters:
+ *      ArrayList<MovieModel> getMovies() - get list of movies similar to a given movie
+ */
+/**************************************************************************************************/
+/**
+ * ArrayList<MovieModel> getMovies() - get list of movies similar to a given movie
+ * @return - list of movie data similar to a given movie
+ */
+    public ArrayList<MovieModel> getMovies(){ return movies; }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Setters:
+ *      - None -
+ */
+/**************************************************************************************************/
+
+    //setter methods
+
+/**************************************************************************************************/
 
 }
