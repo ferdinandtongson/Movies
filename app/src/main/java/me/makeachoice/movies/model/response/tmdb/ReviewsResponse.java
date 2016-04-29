f8987f685593eff1e3899c934efa7b10c566bc69
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-29 10:44:03 -0700

    Add comments to ReviewsResponse

--- app/src/main/java/me/makeachoice/movies/model/response/tmdb/ReviewsResponse.java
+++ app/src/main/java/me/makeachoice/movies/model/response/tmdb/ReviewsResponse.java
@@ -5,15 +5,72 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 
 /**
- * Created by Usuario on 4/23/2016.
+ * ReviewsResponse is used by Gson to parse Review data for a given movie
+ *
+ * API Call to TMDB:
+ *      http://api.themoviedb.org/3/movie/movieID/reviews?api_key=xxx (movieID = id number of movie)
+ *      http://api.themoviedb.org/3/movie/157336/reviews?api_key=xxx
+ * JSON Response:
+ *      root{1}
+ *          array{5}
+ *              id:
+ *              page:
+ *              results[]
+ *              total_pages:
+ *              total_results:
+ *
  */
 public class ReviewsResponse {
-    public int id;
 
+/**************************************************************************************************/
+/**
+ * Class Variables:
+ *      ArrayList<ReviewModel> - list of ReviewModels that are for the given movie
+ *
+ * NOTE: Variable names need to match the JSON response for GSON to properly map the data into
+ * VideoResponse. If using a different variable name, use @SerializedName("")
+ */
+/**************************************************************************************************/
+
+    //reviews - review models that are for the given movie
     @SerializedName("results")
-    public ArrayList<ReviewModel> reviews;
+    private ArrayList<ReviewModel> reviews;
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * ReviewsResponse - constructor, initialize ArrayLists (potential bug if not initialized)
+ */
 
     public ReviewsResponse(){
         reviews = new ArrayList<>();
     }
+
+/**************************************************************************************************/
+/**
+ * Getters:
+ *      ArrayList<ReviewModel> getReviews() - get list of reviews for a given movie
+ */
+/**************************************************************************************************/
+/**
+ * ArrayList<ReviewModel> getReviews() - get list of reviews for a given movie
+ * @return - list of review data for a given movie
+ */
+
+    public ArrayList<ReviewModel> getReviews(){ return reviews; }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Setters:
+ *      - None -
+ */
+/**************************************************************************************************/
+
+    //setter methods
+
+/**************************************************************************************************/
+
 }
