9757719ddda1338c712d8fdbbc0c60d14742c7e1
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-30 13:20:49 -0700

    Set class variables as private and create Setter methods fro MovieItem

--- app/src/main/java/me/makeachoice/movies/model/item/MovieItem.java
+++ app/src/main/java/me/makeachoice/movies/model/item/MovieItem.java
@@ -10,60 +10,68 @@ import me.makeachoice.movies.model.response.tmdb.GenreModel;
  * Created by Usuario on 4/22/2016.
  */
 public class MovieItem {
-    int id;
-    String title;
-    String overview;
-    String releaseDate;
-    String imbdId;
+    private int id;
+    private String title;
+    private String overview;
+    private String releaseDate;
+    private String imbdId;
+    private String homepage;                    //specific movie request
+
+    private String originalTitle;
+    private String originalLanguage;
+
+    private float popularity;
+    private Integer voteCount;
+    private float voteAverage;
+
+    private String posterPath;
+    private String backdropPath;
+    private Boolean video;
 
 
-    String originalTitle;
-    String originalLanguage;
-
-
-    float popularity;
-    float voteCount;
-    float voteAverage;
-
-
-    String posterPath;
-    String backdropPath;
-    Boolean video;
-    String homepage;                    //specific movie request
-
-
-    Boolean adult;
-    Integer[] genreIds;                 //called during a movie list request
-    ArrayList<GenreModel> genres;       //called during a request for a specific movie
+    private Boolean adult;
+    private Integer[] genreIds;                 //called during a movie list request
+    private ArrayList<GenreItem> genres;       //called during a request for a specific movie
+    private ArrayList<CastItem> cast;
 
 
     public int getId(){
         return id;
     }
+    public void setId(int id){ this.id = id; }
 
     public String getTitle(){
         return title;
     }
+    public void setTitle(String title){ this.title = title; }
 
     public String getOverview(){
         return overview;
     }
+    public void setOverview(String overview){ this.overview = overview; }
 
     public String getReleaseDate(){
         return releaseDate;
     }
+    public void setReleaseDate(String date){ this.releaseDate = date; }
 
     public String getImbdId() {return imbdId; }
+    public void setImbdId(String id){ this.imbdId = id; }
+
+    public String getHomepage() { return homepage; }
+    public void setHomepage(String url){ this.homepage = url; }
 
 
 
     public String getOriginalTitle(){
         return originalTitle;
     }
+    public void setOriginalTitle(String title){ this.originalTitle = title; }
 
     public String getOriginalLanguage(){
         return originalLanguage;
     }
+    public void setOriginalLanguage(String language){ this.originalTitle = language; }
 
 
 
@@ -71,40 +79,50 @@ public class MovieItem {
     public float getPopularity(){
         return popularity;
     }
+    public void setPopularity(float popularity){ this.popularity = popularity; }
 
-    public float getVoteCount(){
+    public Integer getVoteCount(){
         return voteCount;
     }
+    public void setVoteCount(Integer count){this.voteCount = count; }
 
     public float getVoteAverage(){
         return voteAverage;
     }
+    public void setVoteAverage(float average){this.voteAverage = average; }
 
 
 
     public String getPosterPath(){ return posterPath; }
+    public void setPosterPath(String path){ this.posterPath = path; }
 
     public String getBackdropPath(){
         return backdropPath;
     }
+    public void setBackdropPath(String path){ this.backdropPath = path; }
 
     public Boolean getVideo(){
         return video;
     }
-
-    public String getHomepage() { return homepage; }
+    public void setVideo(boolean hasVideo){this.video = hasVideo; }
 
 
 
     public Boolean getAdult(){
         return adult;
     }
+    public void setAdult(boolean isAdult){this.adult = isAdult; }
 
     public Integer[] getGenreIds(){
         return genreIds;
     }
+    public void setGenreIds(Integer[] ids){ this.genreIds = ids; }
 
-    public ArrayList<GenreModel> getGenres(){
+    public ArrayList<GenreItem> getGenres(){
         return genres;
     }
+    public void setGenres(ArrayList<GenreItem> genres){ this.genres = genres; }
+
+    public ArrayList<CastItem> getCast(){ return cast; }
+    public void setCast(ArrayList<CastItem> cast){ this.cast = cast; }
 }
