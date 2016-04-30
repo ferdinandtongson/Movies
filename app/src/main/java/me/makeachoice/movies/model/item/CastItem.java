c3e856c11c59c8990d6c7bd1e4d57fd6ae99a052
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-04-30 13:29:04 -0700

    Add CastItem to git

---
+++ app/src/main/java/me/makeachoice/movies/model/item/CastItem.java
@@ -0,0 +1,16 @@
+package me.makeachoice.movies.model.item;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Usuario on 4/30/2016.
+ */
+public class CastItem {
+
+    public String character;
+
+    public String name;
+
+    public String profilePath;
+
+}
