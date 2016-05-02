0f33c6b8ce6b0ab40752c88e5413f8ad02816e2b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-02 11:56:30 -0700

    Add VideoItem to git

---
+++ app/src/main/java/me/makeachoice/movies/model/item/VideoItem.java
@@ -0,0 +1,11 @@
+package me.makeachoice.movies.model.item;
+
+/**
+ * Created by Usuario on 4/23/2016.
+ */
+public class VideoItem {
+    public String key;
+    public String name;
+    public String site;
+    public int size;
+}
