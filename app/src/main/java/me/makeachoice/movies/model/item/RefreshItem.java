5ca458ba314f5af550f365ae574b2081ced55cb4
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-11 16:57:38 -0700

    Add RefreshItem to git

---
+++ app/src/main/java/me/makeachoice/movies/model/item/RefreshItem.java
@@ -0,0 +1,12 @@
+package me.makeachoice.movies.model.item;
+
+/**
+ * Created by Usuario on 4/30/2016.
+ */
+public class RefreshItem {
+
+    public String movieList;
+
+    public Long dateRefresh;
+
+}
