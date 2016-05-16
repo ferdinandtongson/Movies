afdd15057f7d058b68146e5fac557a9d162497dc
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-16 09:08:49 -0700

    Clarify responsibility of RefreshValet

--- app/src/main/java/me/makeachoice/movies/controller/butler/valet/RefreshValet.java
+++ app/src/main/java/me/makeachoice/movies/controller/butler/valet/RefreshValet.java
@@ -1,108 +1,44 @@
 package me.makeachoice.movies.controller.butler.valet;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
-import java.util.Date;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import me.makeachoice.movies.controller.Boss;
-import me.makeachoice.movies.controller.butler.MyButler;
-import me.makeachoice.movies.controller.butler.worker.DBWorker;
-import me.makeachoice.movies.model.db.MovieDB;
 import me.makeachoice.movies.model.db.contract.RefreshContract;
 import me.makeachoice.movies.model.item.RefreshItem;
-import me.makeachoice.movies.util.DateManager;
 
 /**
- * RefreshValet takes care of all the database request.
+ * RefreshValet takes care of all the database request to the movie refresh table.
  *
- * Variables from MyButler:
- *      Boss mBoss
- *      Boolean mWorking
- *      ArrayList<Integer> mRequestBuffer
- *
- * Abstract Methods from MyButler:
- *      abstract public Context getActivityContext()
- *      abstract public void workComplete(Boolean)
  */
-public class RefreshValet extends MyButler implements DBWorker.Bridge {
+public class RefreshValet{
 
 /**************************************************************************************************/
 /**
  * Class Variables:
+ *      RefreshContract mRefreshContract - contract class for the movie refresh table
  */
 /**************************************************************************************************/
 
-    //class variables
+    //mRefreshContract - contract class for the movie refresh table
+    private RefreshContract mRefreshContract;
+
 
 /**************************************************************************************************/
 
-    private final static int DB_INIT = 0;
-    private final static int DB_OPEN = 1;
-
-    private int mRequest;
-    private MovieDB mMovieDB;
-    private RefreshContract mRefreshContract;
-
-    private DBWorker mWorker;
-    private SQLiteDatabase mDB;
 
 /**************************************************************************************************/
 /**
- * _templateButler - constructor, registers to Boss, initialize URI builder, get API key and
- * initialize data buffers, if any
+ * RefreshVale - constructor
  * @param boss - Boss class
  */
     public RefreshValet(Boss boss){
-        //Application context
-        mBoss = boss;
-
-        //initialize access to db
-        mMovieDB = new MovieDB(mBoss);
-
-        mWorker = new DBWorker(this, mMovieDB);
 
         mRefreshContract = new RefreshContract();
 
-        mRefreshMap = new HashMap<>();
-        mRefreshStatus = new HashMap<>();
-
-        //flag to check if work is being done in the background
-        mWorking = false;
-
-        //initialize buffers
-        initBuffers();
-    }
-
-    public void requestDB(){
-        Log.d("Movies", "RefreshValet.requestDB");
-        if(!mWorking){
-            Log.d("Movies", "     start worker");
-            mRequest = DB_OPEN;
-            mWorker.execute();
-        }
-    }
-
-    public void initDB(){
-        if(!mWorking){
-            mRequest = DB_INIT;
-            mWorker.execute();
-        }
-    }
-
-/**
- * void initBuffers() - initialize buffers if any
- */
-    private void initBuffers(){
-        //initialize buffer to hold pending movie requests
-        mRequestBuffer = new ArrayList<>();
-
-        //initialize any other buffers here
-        //initSomeOtherBuffer();
     }
 
 /**************************************************************************************************/
@@ -110,153 +46,102 @@ public class RefreshValet extends MyButler implements DBWorker.Bridge {
 /**************************************************************************************************/
 /**
  * Getters:
- *      Context getActivityContext() - get current Activity context
+ *      HashMap<String, RefreshItem> getRefreshMap() - get refreshMap from database
  *
  * Setters:
- *      - None -
+ *      void setRefresh(SQLiteDatabase,RefreshItem) - save refresh item to database
  */
 /**************************************************************************************************/
 /**
- * Context getActivityContext() - get current Activity context
- * @return - activity context
+ * HashMap<String, RefreshItem> getRefreshMap() - get refreshMap from database
  */
-    public Context getActivityContext() {
-        return mBoss.getActivityContext();
+    public HashMap<String, RefreshItem> getRefreshMap(SQLiteDatabase db){
+        //get Select All query for the movie refresh table
+        String query = RefreshContract.RefreshEntry.SELECT_ALL;
+
+        //run query, get cursor
+        Cursor cursor = db.rawQuery(query, null);
+
+        //check if cursor is null or cursor count is zero
+        if(cursor != null && cursor.getCount() != 0){
+            //cursor is valid, prepare refresh hashMap
+            return prepareRefreshMap(cursor);
+        }
+
+        //invalid cursor, return empty hashMap
+        return new HashMap<>();
     }
+
+/**
+ * void setRefresh(SQLiteDatabase,RefreshItem) - save refresh item into database
+ * @param db - SQLiteDatabase object
+ * @param item - refresh item to save
+ */
+    public void setRefresh(SQLiteDatabase db, RefreshItem item){
+        String whereClause = RefreshContract.RefreshEntry.COLUMN_NAME_MOVIES_LIST + " = '" +
+                item.movieList + "'";
+
+        db.update(RefreshContract.RefreshEntry.TABLE_NAME,
+                mRefreshContract.getContentValues(item), whereClause, null);
+    }
+
+
 
 /**************************************************************************************************/
 
 /**************************************************************************************************/
 /**
  * Class methods
- *      void workComplete(boolean) - called when AsyncTask has completed
+ *      HashMap<String, RefreshItem> prepareRefreshMap(Cursor) - get refresh data from the database
  */
 /**************************************************************************************************/
 /**
- * void workComplete(Boolean) - called when an AsyncTask completes its' work
- * @param result - returns boolean result of task
+ * HashMap<String, RefreshItem> prepareRefreshMap(Cursor) - get refresh data from the database and
+ * put them in a hashMap
+ * @param cursor - cursor containing refresh data from database
+ * @return - refresh item hashMap
  */
-    public void workComplete(Boolean result) {
-        //work has finished
-        mWorking = false;
+    private HashMap<String, RefreshItem> prepareRefreshMap(Cursor cursor){
+        //initialize refresh buffer
+        HashMap<String, RefreshItem> refreshMap = new HashMap<>();
 
-        //do something with data
+        //get number of movie lists to refresh
+        int count = cursor.getCount();
 
-    }
-
-    public void databaseReady(SQLiteDatabase db){
-        Log.d("Movies", "RefreshValet.databaseReady: " + db.toString());
-        //work has finished
-        mWorking = false;
-        mDB = db;
-        closeDatabase();
-    }
-
-    public void closeDatabase(){
-        if(mDB.isOpen()){
-            mDB.close();
-        }
-    }
-
-    public boolean refreshList(String movieList){
-        return mRefreshStatus.get(movieList);
-    }
-
-    private HashMap<String, Boolean> mRefreshStatus;
-    private HashMap<String, RefreshItem> mRefreshMap;
-
-    public boolean refreshPosters(int request){
-        String movieList = mBoss.getString(request);
-        Log.d("Movies", "RefreshValet.checkRefresh: " + movieList);
-        //get refreshItem from HashMap buffer
-        RefreshItem item = mRefreshMap.get(movieList);
-
-        if(item == null){
-            //no data in HashMap, check database for refresh data
-            return checkRefreshInDB(movieList);
-        }
-        else{
-
-            return true;
-        }
-    }
-
-    private boolean checkRefreshInDB(String movieList){
-        //create query to check database for refresh data
-        String query = mRefreshContract.getQueryMovieRefresh(movieList);
-
-        //get database
-        mDB = mMovieDB.getReadableDatabase();
-
-        //run query, get cursor
-        Cursor cursor = mDB.rawQuery(query, null);
-
-        if(cursor != null && cursor.getCount() != 0){
+        //loop through list
+        for(int i = 0; i < count; i++){
             //valid query, move to first item in cursor
             cursor.moveToFirst();
 
-            //get refresh time
-            Long refreshMillis = cursor
-                    .getLong(RefreshContract.RefreshEntry.INDEX_DATE_REFRESH);
+            //create refresh item object
+            RefreshItem item = new RefreshItem();
+            //get movie list name
+            item.movieList = cursor.getString(RefreshContract.RefreshEntry.INDEX_MOVIE_LIST);
+            //get refresh date
+            item.dateRefresh = cursor.getLong(RefreshContract.RefreshEntry.INDEX_DATE_REFRESH);
 
-            return validRefreshDate(movieList, refreshMillis);
+            Log.d("Refresh", "     index 0: " + cursor.getInt(0));
+            Log.d("Refresh", "     index 1: " + cursor.getString(1));
+            Log.d("Refresh", "     index 2: " + cursor.getLong(2));
 
+            //add refreshItem to hashMap with movieList name as key
+            refreshMap.put(item.movieList, item);
+
+            //move to next cursor
+            cursor.moveToNext();
         }
-        else{
-            //TODO - need to handle case where movieList is invalid (??)
-            //cursor equals null, return true;
-            return true;
-        }
+
+        //close cursor
+        cursor.close();
+
+        //return hashMap
+        return refreshMap;
     }
 
-    private boolean validRefreshDate(String movieList, long refreshMillis){
-        //get date of refresh time
-        Date refreshDate = new Date(refreshMillis);
-        //get current date
-        Date currentDate = DateManager.currentDate();
 
-        //check if current date is pass refresh date
-        if(currentDate.after(refreshDate)){
-            //need to refresh, create new refresh date
-            refreshDate = DateManager.addDaysToDate(currentDate, 1);
 
-            //create sql update string for database
-            String updateRefresh = mRefreshContract.getUpdateMovieRefresh(movieList,
-                    currentDate.getTime(), refreshDate.getTime());
 
-            //execute sql, update refresh table with new data
-            mDB.execSQL(updateRefresh);
-
-            //close database
-            closeDatabase();
-
-            //add new refresh data to HashMap buffer
-            addToRefreshMap(movieList, refreshDate.getTime());
-
-            //update refresh status buffer
-            mRefreshStatus.put(movieList, true);
-
-            return true;
-        }
-        else{
-            //no need to refresh database only need to update HashMap buffer
-            addToRefreshMap(movieList, refreshMillis);
-
-            //update refresh status buffer
-            mRefreshStatus.put(movieList, false);
-            return false;
-        }
-
-    }
-
-    private void addToRefreshMap(String movieList, long refreshTime){
-        RefreshItem item = new RefreshItem();
-        item.movieList = movieList;
-        item.dateRefresh = refreshTime;
-
-        mRefreshMap.put(movieList, item);
-
-    }
 /**************************************************************************************************/
+
 
 }
