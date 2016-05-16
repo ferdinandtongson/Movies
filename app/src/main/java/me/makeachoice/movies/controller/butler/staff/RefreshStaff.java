5a89a1bf886012fd8e93c2408e259a517c7f8498
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-16 09:15:51 -0700

    Clean up logic and comments in RefreshStaff

--- app/src/main/java/me/makeachoice/movies/controller/butler/staff/RefreshStaff.java
+++ app/src/main/java/me/makeachoice/movies/controller/butler/staff/RefreshStaff.java
@@ -35,7 +35,6 @@ public class RefreshStaff {
  */
     public RefreshStaff(Boss boss){
         Log.d("Start", "     RefreshStaff - constructor");
-
         //buffer holding movie refresh item data
         mRefreshMap = new HashMap<>();
     }
@@ -48,6 +47,7 @@ public class RefreshStaff {
  *      RefreshItem getRefreshItem(String) - get refreshItem for a given movie list
  *
  * Setters:
+ *      void setRefreshMap(HashMap<String,RefreshItem) - set refreshMap to given map
  *      void setRefreshItem(String,RefreshItem) - save refreshItem to buffer
  */
 /**************************************************************************************************/
@@ -61,11 +61,19 @@ public class RefreshStaff {
     }
 
 /**
+ * void setRefreshMap(HashMap<String,RefreshItem>) - set refreshMap to given map
+ * @param refreshMap - refreshMap to save to buffer
+ */
+    public void setRefreshMap(HashMap<String, RefreshItem> refreshMap){
+        mRefreshMap = new HashMap<>(refreshMap);
+    }
+
+/**
  * void setRefreshItem(String,RefreshItem) - save refreshItem to buffer
  * @param movieList - which movieList has been refreshed
  * @param item - refreshItem to save to buffer
  */
-    public void updateRefreshItem(String movieList, RefreshItem item){
+    public void setRefreshItem(String movieList, RefreshItem item){
         //save refreshItem to buffer
         mRefreshMap.put(movieList, item);
     }
@@ -75,23 +83,37 @@ public class RefreshStaff {
 /**************************************************************************************************/
 /**
  * Class methods
+ *      boolean needToRefreshList(String) - returns true if the list needs to be refreshed
  *      void onFinish() - nulls all of the data in the arrayList buffers
  */
 /**************************************************************************************************/
-
+/**
+ * boolean needToRefreshList(String) - returns true if the list needs to be refreshed. Takes
+ * the current date and compares it with the refresh date. If the current date is pass the
+ * refresh date, the list needs to be updated with current data
+ * @param movieList - movie list name
+ * @return - returns true if list needs to be refreshed, false if not
+ */
     public boolean needToRefreshList(String movieList){
         //get current date
         Date currentDate = DateManager.currentDate();
 
+        //get refresh item movie list
         RefreshItem item = mRefreshMap.get(movieList);
+
+        //check if item is not null
         if(item != null){
+            //valid item, get refresh date
             Date refreshDate = new Date(item.dateRefresh);
 
+            //compare current date with refresh date
             if(currentDate.after(refreshDate)){
+                //refresh date has already passed, need to refresh
                 return true;
             }
         }
 
+        //refresh date is still in the future, NO need to refresh
         return false;
     }
 
