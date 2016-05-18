fcddb4192dc5491f0f62c1327c495a6b7feab6ae
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-17 21:50:30 -0700

    Add resource variables to SwipeHelper

--- app/src/main/java/me/makeachoice/movies/controller/housekeeper/helper/SwipeHelper.java
+++ app/src/main/java/me/makeachoice/movies/controller/housekeeper/helper/SwipeHelper.java
@@ -3,7 +3,7 @@ package me.makeachoice.movies.controller.housekeeper.helper;
 import me.makeachoice.movies.R;
 
 /**
- * SimpleHelper contains constant definitions for all the resources used by SwipeActivity. It also
+ * SwipeHelper contains constant definitions for all the resources used by SwipeActivity. It also
  * has a ViewHolder pattern implementation contained in the inner class inherited from MyHelper
  * abstract class
  *
@@ -33,7 +33,7 @@ public class SwipeHelper extends MyHelper{
  * container views that will display fragments. Also, toolbar and floating action button layout
  * ids should be put here. These are all found in res/layout
  */
-    /**************************************************************************************************/
+/**************************************************************************************************/
 
     //SWIPE_LAYOUT_ID is the layout used to display fragments
     public static final int SWIPE_LAYOUT_ID = R.layout.activity_swipe;
@@ -44,33 +44,20 @@ public class SwipeHelper extends MyHelper{
     //SWIPE_TITLE_STRIP_ID is the viewpager title strip
     public static final int SWIPE_TITLE_STRIP_ID = R.id.swipe_title_strip;
 
-    //SWIPE_TOOLBAR_ID is the toolbar used by Activity
-    public static final  int SWIPE_TOOLBAR_ID = R.id.toolbar;
-
-    //XXX_FAB_ID is the floating action button
-    //public static final  int XXX_FAB_ID = R.id.fab;
+    //SWIPE_FAB_ID is the floating action button
+    public static final  int SWIPE_FAB_ID = R.id.swipe_fab;
 
 /**************************************************************************************************/
+
 
 /**************************************************************************************************/
 /**
- * Menu ids used in toolbar of the Activity.
- *
- * Note:
- * Will only need to define these values if the Activity will contain a toolbar.
+ * String ids used by the Activity
  */
 /**************************************************************************************************/
 
-    //TODO - create menu file for toolbar
-    //Menu for Toolbar found in menu_main menu file
-    public static final  int SWIPE_MENU = R.menu.menu_swipe;
+    public static final int KEY_MOVIE_INDEX = R.string.key_movie_index;
 
-    //TODO - define menu items
-    //Item id from Menu
-    public static final  int MENU_ITEM01 = R.id.bar_settings;
-    //public static final  int MENU_ITEM02 = R.id.action_bar_item02;
-    //public static final  int MENU_ITEM03 = R.id.action_bar_item03;
-    //public static final  int MENU_ITEM04 = R.id.action_bar_item04;
 
 /**************************************************************************************************/
 
