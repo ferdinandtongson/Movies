74afb2e3f781f0da31366b41ef5707d35d5ae241
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-18 23:55:38 -0700

    Add bitmap and byte[] variables to PosterItem

--- app/src/main/java/me/makeachoice/movies/model/item/PosterItem.java
+++ app/src/main/java/me/makeachoice/movies/model/item/PosterItem.java
@@ -1,11 +1,12 @@
 package me.makeachoice.movies.model.item;
 
+
 import android.graphics.Bitmap;
 
 /**
  * PosterItem extends MyItem base class and will be used by a BaseAdapter.
  *
- * PosterItem will contain the title and source image of the poster
+ * PosterItem will contain the poster data used by View
  *
  * In your Presenter, adding data to your adapter before calling setListAdapter():
  *      ArrayList<T> rowItems = new ArrayList<>();
@@ -29,8 +30,10 @@ public class PosterItem extends MyItem {
     protected String mTitle;
     //mPosterPath - holds the path to the poster image
     protected String mPosterPath;
-    //mImage - holds the poster image
-    protected Bitmap mImage;
+    //mPoster - holds the poster image
+    protected Bitmap mPoster;
+    //mPosterByte - holds the byte array for the poster image
+    protected byte[] mPosterByte;
 
 /**************************************************************************************************/
 /**
@@ -40,6 +43,7 @@ public class PosterItem extends MyItem {
 
     /**
      * PosterItem(String, int) - constructor
+     * @param id - TheMovieDB id of the movie
      * @param title - title of poster
      * @param path - path to image of poster
      */
@@ -51,33 +55,98 @@ public class PosterItem extends MyItem {
         //path to poster image
         mPosterPath = path;
         //default image is null
-        mImage = null;
+        mPoster = null;
+        //default image byte array
+        mPosterByte = null;
     }
 
 /**************************************************************************************************/
+
+/**************************************************************************************************/
 /**
- * Getter and Setter for the image id of the movie poster thumbnail
- * int getTMDBId() returns TheMovieDB id number of the movie
- * String getTitle() returns title of poster
- * int getPosterPath() returns the poster id
- *
- * void setTMDBId(int) sets TheMovieDB id number of the movie
- * void setTitle(String) sets title value of poster
- * void setPoster(int) sets the int value of the poster source id
+ * Getters:
+ *      int getTMDBId() - gets TheMovieDB id number of the movie
+ *      String getTitle() - gets title of poster
+ *      String getPosterPath() - gets the full API path to the poster image in TMDB
+ *      Bitmap getPoster() - gets the poster bitmap
+ *      byte[] getPosterBytes() - gets the byte array of the poster bitmap
+ */
+/**************************************************************************************************/
+/**
+ * int getTMDBId() - gets TheMovieDB id number of the movie
+ * @return - movie id number
  */
     public int getTMDBId(){ return mTMDBId; }
+
+/**
+ * String getTitle() - gets title of poster
+ * @return - title of poster
+ */
     public String getTitle() { return mTitle; }
+
+/**
+ * String getPosterPath() - gets the full API path to the poster image in TMDB
+ * @return - poster path API call
+ */
     public String getPosterPath(){
         return mPosterPath;
     }
-    public Bitmap getImage(){ return mImage; }
 
+/**
+ * Bitmap getPoster() - gets the poster bitmap
+ * @return - bitmap of poster
+ */
+    public Bitmap getPoster(){ return mPoster; }
+
+/**
+ * byte[] getPosterBytes() - gets the byte array of the poster bitmap
+ * @return - byte array of poster bitmap
+ */
+    public byte[] getPosterBytes(){ return mPosterByte; }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Setters:
+ *      void setTMDBId(int) - sets TheMovieDB id number of the movie
+ *      void setTitle(String) - sets title value of the poster
+ *      void setPosterPath(String) - sets the full poster path API call to TMDB
+ *      void setPoster(Bitmap) - sets the bitmap value of the poster
+ *      void setPosterBytes(byte[]) - set the byte array value of the poster bitmap
+ */
+/**************************************************************************************************/
+/**
+ * void setTMDBId(int) - sets TheMovieDB id number of the movie
+ * @param id - TMDB id number of movie
+ */
     public void setTMDBId(int id){ mTMDBId = id; }
+
+/**
+ * void setTitle(String) - sets the title value of the poster
+ * @param title - title of poster
+ */
     public void setTitle(String title) { mTitle = title; }
+
+/**
+ * void setPosterPath(String) - sets the full poster path API call to TMDB
+ * @param path - full poster path API call
+ */
     public void setPosterPath(String path){
         mPosterPath = path;
     }
-    public void setImage(Bitmap img) { mImage = img; }
+
+/**
+ * void setPoster(Bitmap) - sets the bitmap value of the poster
+ * @param bmp - bitmap of the poster
+ */
+    public void setPoster(Bitmap bmp) { mPoster = bmp; }
+
+/**
+ * void setPosterBytes(byte[]) - set the byte array value of the poster bitmap
+ * @param bmpByte - byte array of the poster bitmap
+ */
+    public void setPosterBytes(byte[] bmpByte){ mPosterByte = bmpByte;}
 
 /**************************************************************************************************/
 }
