218d8cb402cf89bf4c7c847a077ce55aaad4e3ec
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-05-24 12:21:43 -0700

    Add bitmap and byte[] methods to MovieContract

--- app/src/main/java/me/makeachoice/movies/model/db/contract/MovieContract.java
+++ app/src/main/java/me/makeachoice/movies/model/db/contract/MovieContract.java
@@ -1,14 +1,46 @@
 package me.makeachoice.movies.model.db.contract;
 
 import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.BaseColumns;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 
 import me.makeachoice.movies.model.item.MovieItem;
 
 /**
- * Created by Usuario on 5/7/2016.
+ * MovieContract - is the database contract class for Movie tables
  */
 public final class MovieContract extends MyContract{
+
+/**************************************************************************************************/
+/**
+ * Static INDEX Variables:
+ *      int INDEX_ID - column index of primary key
+ *      int INDEX_ORDER - column index of movie order
+ *      int INDEX_MOVIE_ID - column index of the id number of TheMovieDB
+ *      int INDEX_TITLE - column index of the title of movie
+ *      int INDEX_OVERVIEW - column index of the overview of the movie
+ *      int INDEX_RELEASE_DATE - column index of the release date of the movie
+ *      int INDEX_IMDB_ID - column index of the id number of InternetMovieDB
+ *      int INDEX_HOME_PAGE - column index of the homepage of the movie
+ *
+ *      int INDEX_ORIGINAL_TITLE - column index of the original title of the movie
+ *      int INDEX_ORIGINAL_LANGUAGE - column index of the original language of the movie
+ *
+ *      int INDEX_POPULARITY - column index of the popularity ranking of the movie
+ *      int INDEX_VOTE_COUNT - column index of the number of votes the movie has
+ *      int INDEX_VOTE_AVERAGE - column index of the average rating the movie has
+ *
+ *      int INDEX_POSTER_PATH - column index of the full url path to poster of the movie
+ *      int INDEX_POSTER_BYTES - column index of the byte array of the poster image
+ */
+/**************************************************************************************************/
+
     public static final int INDEX_ID = 0;
     public static final int INDEX_ORDER = 1;
     public static final int INDEX_MOVIE_ID = 2;
@@ -26,7 +58,32 @@ public final class MovieContract extends MyContract{
     public static final int INDEX_VOTE_AVERAGE = 12;
 
     public static final int INDEX_POSTER_PATH = 13;
-    public static final int INDEX_POSTER = 14;
+    public static final int INDEX_POSTER_BYTES = 14;
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Static COLUMN_NAME Variables:
+ *      int COLUMN_NAME_ORDER - column name for movie order
+ *      int COLUMN_NAME_MOVIE_ID - column name for the id number of TheMovieDB
+ *      int COLUMN_NAME_TITLE - column name for the title of movie
+ *      int COLUMN_NAME_OVERVIEW - column name for the overview of the movie
+ *      int COLUMN_NAME_RELEASE_DATE - column name for the release date of the movie
+ *      int COLUMN_NAMEX_IMDB_ID - column name for the id number of InternetMovieDB
+ *      int COLUMN_NAME_HOME_PAGE - column name for the homepage of the movie
+ *
+ *      int COLUMN_NAME_ORIGINAL_TITLE - column name for the original title of the movie
+ *      int COLUMN_NAME_ORIGINAL_LANGUAGE - column name for the original language of the movie
+ *
+ *      int COLUMN_NAME_POPULARITY - column name for the popularity ranking of the movie
+ *      int COLUMN_NAME_VOTE_COUNT - column name for the number of votes the movie has
+ *      int COLUMN_NAME_VOTE_AVERAGE - column name for the average rating the movie has
+ *
+ *      int COLUMN_NAME_POSTER_PATH - column name for the full url path to poster of the movie
+ *      int COLUMN_NAME_POSTER_BYTES - column name for the byte array of the poster image
+ */
+/**************************************************************************************************/
 
     public static final String COLUMN_NAME_ORDER = "movieOrder";
     public static final String COLUMN_NAME_MOVIE_ID = "movieId";
@@ -44,18 +101,141 @@ public final class MovieContract extends MyContract{
     public static final String COLUMN_NAME_VOTE_AVERAGE = "voteAverage";
 
     public static final String COLUMN_NAME_POSTER_PATH = "posterPath";
-    public static final String COLUMN_NAME_POSTER = "poster";
-
-    // To prevent someone from accidentally instantiating the contract class,
-    // give it an empty constructor.
-    public MovieContract() {}
-
+    public static final String COLUMN_NAME_POSTER_BYTES = "posterByte";
 
 /**************************************************************************************************/
 
 /**************************************************************************************************/
 /**
- * MostPopularEntry - list of most popular movies
+ * MovieContract - construct
+ */
+    public MovieContract() {}
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/**
+ * Class Methods:
+ *      ContentValues getContentValues(MovieItem,int) - convert MovieItem data to ContentValues
+ *      Bitmap getBitmapFromURL(String) - gets poster image from API call
+ *      byte[] convertBitmap(Bitmap) - convert bitmap image into byte[]
+ */
+/**************************************************************************************************/
+/**
+ * ContentValues getContentValues(MovieItem,int) - convert MovieItem data to ContentValues. If
+ * there is no byte[] data, will make an API call to download the bitmap poster and convert it
+ * to a byte[] to store in the database
+ * @param item - movie item data
+ * @param index - order position of movie item in list
+ * @return - ContentValues of movie item
+ */
+    public ContentValues getContentValues(MovieItem item, int index){
+        //create ContentValues object
+        ContentValues values = new ContentValues();
+
+        //add movie item data to ContentValues
+        values.put(MovieContract.COLUMN_NAME_MOVIE_ID, item.getTMDBId());
+        values.put(MovieContract.COLUMN_NAME_ORDER, index);
+        values.put(MovieContract.COLUMN_NAME_TITLE, item.getTitle());
+        values.put(MovieContract.COLUMN_NAME_OVERVIEW, item.getOverview());
+        values.put(MovieContract.COLUMN_NAME_RELEASE_DATE, item.getReleaseDate());
+        values.put(MovieContract.COLUMN_NAME_IMBD_ID, item.getIMDBId());
+        values.put(MovieContract.COLUMN_NAME_HOME_PAGE, item.getHomepage());
+
+        values.put(MovieContract.COLUMN_NAME_ORIGINAL_TITLE, item.getOriginalTitle());
+        values.put(MovieContract.COLUMN_NAME_ORIGINAL_LANGUAGE, item.getOriginalLanguage());
+
+        values.put(MovieContract.COLUMN_NAME_POPULARITY, item.getPopularity());
+        values.put(MovieContract.COLUMN_NAME_VOTE_COUNT, item.getVoteCount());
+        values.put(MovieContract.COLUMN_NAME_VOTE_AVERAGE, item.getVoteAverage());
+
+        values.put(MovieContract.COLUMN_NAME_POSTER_PATH, item.getPosterPath());
+
+        //check if movie item has a byte[] data
+        if(item.getPosterBytes() != null){
+            //has byte[] data, save to ContentValues
+            values.put(MovieContract.COLUMN_NAME_POSTER_BYTES, item.getPosterBytes());
+        }
+        else{
+            //no byte[] data, get bitmap poster from internet
+            Bitmap poster = getBitmapFromURL(item.getPosterPath());
+
+            //check if poster is null
+            if(poster != null){
+                //not null, save bitmap poster to movie item
+                item.setPoster(poster);
+
+                //convert bitmap to byte[] data
+                byte[] bytePoster = convertBitmap(poster);
+
+                //save byte[] to movie item
+                item.setPosterBytes(bytePoster);
+
+                //save byte[] to ContentValues
+                values.put(MovieContract.COLUMN_NAME_POSTER_BYTES, bytePoster);
+            }
+        }
+
+        //return ContentValues
+        return values;
+    }
+
+/**
+ * byte[] getBitmapFromURL(PosterItem) - gets poster image from API call. Uses the poster path
+ * url to download the poster image.
+ * @param posterPath - url poster path to poster image
+ * @return - bitmap of poster image
+ */
+    private Bitmap getBitmapFromURL(String posterPath){
+        try {
+            //create url object with poster path string (complete API call to TMDB)
+            java.net.URL url = new java.net.URL(posterPath);
+
+            //open connection to url
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+            //allow for DoInput
+            connection.setDoInput(true);
+
+            //attempt url connection
+            connection.connect();
+
+            //get InputStream from url connection
+            InputStream input = connection.getInputStream();
+
+            //convert InputStream into bitmap
+            return BitmapFactory.decodeStream(input);
+
+        } catch (IOException e) {
+            //IOException event happened
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+/**
+ * byte[] convertBitmap(Bitmap) - convert bitmap image into byte[].
+ * @param image - bitmap image to convert into byte[]
+ * @return - byte[] of image
+ */
+    private byte[] convertBitmap(Bitmap image){
+        //create byt[] array output stream
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        //compress bitmap image into byte[]
+        image.compress(Bitmap.CompressFormat.PNG, 100, bos);
+
+        //return byte[]
+        return bos.toByteArray();
+    }
+
+
+/**************************************************************************************************/
+
+
+/**************************************************************************************************/
+/**
+ * MostPopularEntry - table implementation for Most Popular movies
  */
 /**************************************************************************************************/
 
@@ -77,7 +257,7 @@ public final class MovieContract extends MyContract{
                 COLUMN_NAME_VOTE_COUNT + MyContract.INTEGER_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_VOTE_AVERAGE + MyContract.REAL_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_POSTER_PATH + MyContract.TEXT_TYPE + MyContract.COMMA_SEP +
-                COLUMN_NAME_POSTER + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
+                COLUMN_NAME_POSTER_BYTES + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
 
         public static final String DROP_TABLE = MyContract.DROP_TABLE + TABLE_NAME;
 
@@ -89,7 +269,7 @@ public final class MovieContract extends MyContract{
 
 /**************************************************************************************************/
 /**
- * TopRatedEntry - list of top rated movies
+ * TopRatedEntry - table implementation for Top Ratedd movies
  */
 /**************************************************************************************************/
 
@@ -111,7 +291,7 @@ public final class MovieContract extends MyContract{
                 COLUMN_NAME_VOTE_COUNT + MyContract.INTEGER_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_VOTE_AVERAGE + MyContract.REAL_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_POSTER_PATH + MyContract.TEXT_TYPE + MyContract.COMMA_SEP +
-                COLUMN_NAME_POSTER + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
+                COLUMN_NAME_POSTER_BYTES + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
 
         public static final String DROP_TABLE = MyContract.DROP_TABLE + TABLE_NAME;
 
@@ -122,7 +302,7 @@ public final class MovieContract extends MyContract{
 
 /**************************************************************************************************/
 /**
- * NowPlayingEntry - list of movies now playing
+ * NowPlayingEntry - table implementation for Now Playing movies
  */
 /**************************************************************************************************/
 
@@ -144,7 +324,7 @@ public final class MovieContract extends MyContract{
                 COLUMN_NAME_VOTE_COUNT + MyContract.INTEGER_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_VOTE_AVERAGE + MyContract.REAL_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_POSTER_PATH + MyContract.TEXT_TYPE + MyContract.COMMA_SEP +
-                COLUMN_NAME_POSTER + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
+                COLUMN_NAME_POSTER_BYTES + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
 
         public static final String DROP_TABLE = MyContract.DROP_TABLE + TABLE_NAME;
 
@@ -155,7 +335,7 @@ public final class MovieContract extends MyContract{
 
 /**************************************************************************************************/
 /**
- * UpcomingEntry - list of movies now playing
+ * UpcomingEntry - table implementation for Upcoming movies
  */
 /**************************************************************************************************/
 
@@ -177,7 +357,7 @@ public final class MovieContract extends MyContract{
                 COLUMN_NAME_VOTE_COUNT + MyContract.INTEGER_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_VOTE_AVERAGE + MyContract.REAL_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_POSTER_PATH + MyContract.TEXT_TYPE + MyContract.COMMA_SEP +
-                COLUMN_NAME_POSTER + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
+                COLUMN_NAME_POSTER_BYTES + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
 
         public static final String DROP_TABLE = MyContract.DROP_TABLE + TABLE_NAME;
 
@@ -188,7 +368,7 @@ public final class MovieContract extends MyContract{
 
 /**************************************************************************************************/
 /**
- * FavoriteEntry - list of favorite movies
+ * FavoriteEntry - table implementation for Favorite movies
  */
 /**************************************************************************************************/
 
@@ -210,7 +390,7 @@ public final class MovieContract extends MyContract{
                 COLUMN_NAME_VOTE_COUNT + MyContract.INTEGER_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_VOTE_AVERAGE + MyContract.REAL_TYPE + MyContract.COMMA_SEP +
                 COLUMN_NAME_POSTER_PATH + MyContract.TEXT_TYPE + MyContract.COMMA_SEP +
-                COLUMN_NAME_POSTER + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
+                COLUMN_NAME_POSTER_BYTES + MyContract.BLOB_TYPE + MyContract.PAREN_CLOSE;
 
         public static final String DROP_TABLE = MyContract.DROP_TABLE + TABLE_NAME;
 
